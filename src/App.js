33ee1ba8b5ba5a00c5e70160ab96052145aee03f
Markus <markusbeamer@Administrators-MacBook-Pro.local>
2017-08-29 11:32:54 -0400

    finishing challenges

--- src/App.js
+++ src/App.js
@@ -3,19 +3,31 @@ import './App.css';
 import Header from './Header.js';
 import Information from './Information.js';
 var contactList = [
-					{firstName:"jjj", lastName:"ddd", phoneNumber:111},
+					{firstName:"Malik", lastName:"Robinson", phoneNumber:111},
 					{firstName:"kkk", lastName:"ccc", phoneNumber:222},
 					{firstName:"lll", lastName:"sss", phoneNumber:333}	
 					];
-
+// var colorChange = document.getElementById("pending");
 class App extends Component {
+	constructor() {
+    	super();
+    	this.state = {backgroundcolor: "red"};
+    	this.pending = this.pending.bind(this);
 // HARD: Using an array of contacts, loop through each one and create a new Contact component for each
+}
+
+pending(){
+	console.log("test")
+	this.setState({
+	backgroundcolor: "green"
+	});
+}
   render() {
     return (
       <div className="App">
       	<Header name="Markus" />
       	<Information firstName="Alex" lastName="Beamer" phoneNumber="8888888"/>
-      	{contactList.map((contact) => <Information firstName={contact.firstName} lastName={contact.lastName} phoneNumber={contact.phoneNumber} />)}
+      	{contactList.map((contact) => <Information firstName={contact.firstName} lastName={contact.lastName} phoneNumber={contact.phoneNumber} pending={this.pending} color={this.state.backgroundcolor} />)}
       </div>
     );
   }
--- src/Information.js
+++ src/Information.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
   	return(
 
 		//MEDIUM: Create a child Contact component inside the original component you created that you pass in a firstName, lastName, and phoneNumber and print out "You need to contact {firstName} {lastName} at {phoneNumber}
-  		<p>You need to contact {this.props.firstName} {this.props.lastName} at {this.props.phoneNumber} <button>call</button> <span>Pending</span></p>
+  		<p>You need to contact {this.props.firstName} {this.props.lastName} at {this.props.phoneNumber} <button onClick={this.props.pending}>call</button> <span id="pending" style={{backgroundColor: this.props.color}}>Pending</span></p>
   	)
   }
 }
